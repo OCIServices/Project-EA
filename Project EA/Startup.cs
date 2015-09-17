f089051e97193930905ec1f3afaf8a0b00b33fc6
Kenny Peel <knowonehome@gmail.com>
2015-09-17 15:02:29 -0500

    initial commit

---
+++ Project EA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_EA.Startup))]
+namespace Project_EA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
