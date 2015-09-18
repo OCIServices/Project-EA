66c682e4f62317faaa28e6dd5d62f98ac0317815
Kenny Peel <knowonehome@gmail.com>
2015-09-18 11:12:47 -0500

    Added the controller for Broker information which generated the view. and will also auto generate the database needed.

---
+++ Project EA/Controllers/BrokerInformationsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Project_EA.Models;
+
+namespace Project_EA.Controllers
+{
+    public class BrokerInformationsController : Controller
+    {
+        private BrokerInformationDBContext db = new BrokerInformationDBContext();
+
+        // GET: BrokerInformations
+        public ActionResult Index()
+        {
+            return View(db.BrokerInformations.ToList());
+        }
+
+        // GET: BrokerInformations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BrokerInformation brokerInformation = db.BrokerInformations.Find(id);
+            if (brokerInformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brokerInformation);
+        }
+
+        // GET: BrokerInformations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrokerInformations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FirstName,LastName,DOILicenseNumber,Address1,Address2,City,State,Zip,PhoneNumber,FaxNumber,EmailAddress,AgencyName,NameOfGeneralAgent,Payee,PayeeTaxId,SsnTin,BrokerCommissionSplitPercent,IsProducerAppointed,ProducerTitle")] BrokerInformation brokerInformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BrokerInformations.Add(brokerInformation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(brokerInformation);
+        }
+
+        // GET: BrokerInformations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BrokerInformation brokerInformation = db.BrokerInformations.Find(id);
+            if (brokerInformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brokerInformation);
+        }
+
+        // POST: BrokerInformations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,DOILicenseNumber,Address1,Address2,City,State,Zip,PhoneNumber,FaxNumber,EmailAddress,AgencyName,NameOfGeneralAgent,Payee,PayeeTaxId,SsnTin,BrokerCommissionSplitPercent,IsProducerAppointed,ProducerTitle")] BrokerInformation brokerInformation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(brokerInformation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(brokerInformation);
+        }
+
+        // GET: BrokerInformations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BrokerInformation brokerInformation = db.BrokerInformations.Find(id);
+            if (brokerInformation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(brokerInformation);
+        }
+
+        // POST: BrokerInformations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BrokerInformation brokerInformation = db.BrokerInformations.Find(id);
+            db.BrokerInformations.Remove(brokerInformation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Project EA/Models/BrokerInformation.cs
+++ Project EA/Models/BrokerInformation.cs
@@ -1,32 +1,58 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
 namespace Project_EA.Models
 {
     public class BrokerInformation
     {
         public int ID { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
-        public int DOILicenseNumber { get; set; }
+
+        public string DOILicenseNumber { get; set; }
+
         public string Address1 { get; set; }
+
         public string Address2 { get; set; }
+
         public string City { get; set; }
+
         public string State { get; set; }
-        public int Zip { get; set; }
-        public int PhoneNumber { get; set; }
-        public int FaxNumber { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        public string Zip { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string FaxNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+
         public string AgencyName { get; set; }
+
         public string NameOfGeneralAgent { get; set; }
+
         public string Payee { get; set; }
+
         public string PayeeTaxId { get; set; }
-        public int SsnTin { get; set; }
+
+        public string SsnTin { get; set; }
+
         public int BrokerCommissionSplitPercent { get; set; }
+
         public string IsProducerAppointed { get; set; }
+
         public string ProducerTitle { get; set; }
+
     }
 
-    public class BrokerInformationDBContext :ApplicationDbContext
+    public class BrokerInformationDBContext :DbContext
     {
         public DbSet<BrokerInformation> BrokerInformations { get; set; }
     }
